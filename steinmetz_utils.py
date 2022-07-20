da181cd569382179b56f47acd1750d084849f172
Xiaoyue-Zhou <109672938+Xiaoyue-Zhou@users.noreply.github.com>
2022-07-20 22:32:00 +0800

    Update steinmetz_utils.py
    add a new function to do the modeling thing: neuronModel_thisTrialResp(alldat, session, brain_area, till=50, penalty='l2', C=1, cv=16)

--- steinmetz_utils.py
+++ steinmetz_utils.py
@@ -39,3 +39,55 @@ def timecourse_3d(timecourse: np.ndarray) -> None:
     ax = fig.add_subplot(111, projection='3d')
 
     ax.plot(*timecourse)
+    
+ 
+def neuronModel_thisTrialResp(alldat, session, brain_area, till=50,
+                              penalty='l2', C=1, cv=16):
+    '''
+
+    :param alldat: Steinmetz dataset
+    :param session: select from 0~38
+    :param brain_area: needed brain area name. e.g.,'VISa'
+    :param till: the number of time bins needed.
+    :param penalty: 'l1'/'l2' type of penalty for regularization. set to 'l2' by default
+    :param C: strength of penalty if any penalty. C=1/lambda.
+    :param cv: number of folds when doing the cross validation test.
+
+    :return: ori_acc(train set acc) & ori_cvd(test set acc)
+
+    '''
+
+    dat = alldat[session]
+
+    isResp = dat['response'] != 0
+    isNeededArea = dat['brain_area'] == brain_area
+
+    respAll = dat['response']
+    resp = respAll[isResp]     # '-1' means the right stimulus has higher contrast
+    resp = resp.astype('int')
+
+    s = dat['spks']
+    s = s[:,isResp,:till]
+    s = s[isNeededArea,:,:]
+
+    ## compute firing rate - average
+    fRate = np.sum(s,2)
+    fRate = fRate.T
+    dim = fRate.shape[0]
+    # add 1s line
+    fRate = np.concatenate((np.ones(fRate.shape[0]).reshape(dim,1),fRate), axis=1)
+
+    # fit GLM with regularization
+    if penalty == 'l2':
+        model = LogisticRegression(penalty='l2',C=C,max_iter=5000)
+    elif penalty == 'l1':
+        model = LogisticRegression(penalty='l1',C=C,max_iter=5000,solver='saga')
+    else:
+        model = LogisticRegression(penalty=None, max_iter=5000)
+
+    model.fit(fRate,resp)
+
+    ori_acc = compute_acc(fRate, resp, model) # a single number
+    ori_cvd = cross_val_score(model, fRate, resp, cv=cv) # an array (cv,)
+
+    return ori_acc,  ori_cvd
