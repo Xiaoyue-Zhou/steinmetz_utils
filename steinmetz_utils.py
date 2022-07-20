9677821d9197b60e5c8fc6cf80a46bc48f2aecaa
Xiaoyue-Zhou <109672938+Xiaoyue-Zhou@users.noreply.github.com>
2022-07-20 21:12:26 +0800

    Update steinmetz_utils.py
    added a ploting function: plot_by_pretrial_feedback

--- steinmetz_utils.py
+++ steinmetz_utils.py
@@ -39,3 +39,43 @@ def timecourse_3d(timecourse: np.ndarray) -> None:
     ax = fig.add_subplot(111, projection='3d')
 
     ax.plot(*timecourse)
+
+
+    
+def plot_by_pretrial_feedback(s, feedback_type, title = None, normalize = False) -> None:
+    '''
+
+    :param s: data of spike trians: (neurons * trials * time bins)
+    :param feedback_type: an array made up of 1s & -1s. 1 for reward and -1 for penalty.
+    :param title: the title of the plot generated. e.g., the name of brain region.
+    :param normalize：whether normalize the spike data by trials and neurons.
+
+    :return: plot (made by pyplot)
+
+    '''
+
+    isRew = feedback_type == 1
+    isPen = feedback_type == -1
+
+    lastIsRew = isRew[:-1]
+    lastIsPen = isPen[:-1]
+
+    if normalize:
+        for i in range(s.shape[2]):
+            s[:,:,i] = (s[:,:,i] - np.mean(s,2)) / np.std(s,2)
+
+    s = s[:,1:,:]
+    s_lastIsRew = s[:,lastIsRew,:]
+    s_lastIsPen = s[:,lastIsPen,:]
+
+    err_lastIsRew = np.std(np.mean(s_lastIsRew,0),0) / np.sqrt(s_lastIsRew.shape[1]-1)
+    err_lastIsPen = np.std(np.mean(s_lastIsPen,0),0) / np.sqrt(s_lastIsPen.shape[1]-1)
+
+    # draw the plot
+    x = list(range(0,s.shape[2],1))
+    plt.errorbar(x, np.mean(s_lastIsRew, (0,1)), yerr=err_lastIsRew, label='Post Reword')
+    plt.errorbar(x, np.mean(s_lastIsPen, (0,1)), yerr=err_lastIsPen, label='Post Penalty')
+
+    plt.title(title)
+    plt.legend()
+    plt.show()
